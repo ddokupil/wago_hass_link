002296526d880891e3e235ad1f13b4125ecace9b
Darek <ddokupil@gmail.com>
2023-04-04 09:37:25 +0200

    mqtt, logger, service mode, other features

--- script.py
+++ script.py
@@ -7,19 +7,59 @@ from ftplib import FTP
 import sys
 from io import BytesIO
 from collections import OrderedDict
+import json
+import paho.mqtt.publish as publish
+import logging
+import logging.handlers
+import time
+
+# Set up the logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.CRITICAL)
+
+# Define a log format
+log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+# Define a rotating file handler with a max size of 10MB
+file_handler = logging.handlers.RotatingFileHandler('log.txt', maxBytes=10_000_000, backupCount=1)
+file_handler.setFormatter(log_formatter)
+
+# Define a stream handler to print logs to stdout
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(log_formatter)
+
+# Add the handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 response = ""
 prev_response = ""
 parser = argparse.ArgumentParser()
 
 # Define arguments for FTP download
-parser.add_argument('--host', help='Wago address', default='192.168.1.2')
-parser.add_argument('--user', help='FTP server username', default='user')
-parser.add_argument('--password', help='FTP server password', default='user')
-parser.add_argument('--file', help='File path on FTP server', default='/PLC/plc_visu.xml')
+parser.add_argument('-H', '--host', help='Wago address', default='192.168.1.2')
+parser.add_argument('-u', '--user', help='FTP server username', default='user')
+parser.add_argument('-p', '--password', help='FTP server password', default='user')
+parser.add_argument('-f', '--file', help='File path on FTP server', default='/PLC/plc_visu.xml')
+parser.add_argument('-v', '--verbose', action='count', default=0, help='Increase output verbosity (e.g., -v, -vv, -vvv)')
+parser.add_argument('-s', '--service', action='count', help='Whenn connection problem occurs wait 60 seconds before exiting.', default=0)
 args = parser.parse_args()
 
+# Configure logging level based on verbose flag
+if args.verbose == 1:
+    logging.basicConfig(level=logging.INFO)
+elif args.verbose == 2:
+    logging.basicConfig(level=logging.DEBUG)
+elif args.verbose >= 3:
+    logging.basicConfig(level=logging.NOTSET)
+
 endpoint = f"http://{args.host}/PLC/webvisu.htm"
+
+# Define MQTT server parameters
+MQTT_SERVER = "192.168.1.4"
+MQTT_USERNAME = "user"
+MQTT_PASSWORD = "passwd"
+MQTT_TOPIC = "wago/bridge"
 
 # Fetch the XML file from FTP
 ftp = FTP(args.host)
@@ -31,8 +71,12 @@ try:
 
     ftp.quit()
 except Exception as e:
-    print("Failed to download file from FTP:", e)
+    logger.error("Failed to download file from FTP: %s", e)
     ftp.quit()
+    if args.service >= 1:
+        logger.debug(f"Will retry from scratch in a minute.")
+        time.sleep(60)
+    sys.exit()
 
 # Parse XML file
 tree = ET.ElementTree(ET.fromstring(file_content.decode('latin-1')))
@@ -44,13 +88,14 @@ variables = OrderedDict()
 # Parse variables from XML
 for variable in root.findall('variablelist/variable'):
     name = variable.get('name')
+    name = name.lstrip(".")
     value = variable.text.strip()
     variables[name] = value
 
 # Print out the variables as they are in xml file
 for i in range(len(variables)):
     key, value = list(variables.items())[i]
-    print(f"{key} is {value}")
+    logger.debug(f"{key} is {value}")
 
 # Define the payload header
 payload = f"|0|{len(variables)}|"
@@ -63,23 +108,52 @@ for name, value in variables.items():
     counter += int(num_bytes)
 
 # Do for the first time to initialize the previous response
-prev_response = requests.post(endpoint, data=payload)
-
+try:
+    prev_response = requests.post(endpoint, data=payload)
+except Exception as e:
+    logger.critical(f"Can't even make the first request: {e}\nTerminating\n")
+    if args.service >= 1:
+        logger.debug(f"Will retry from scratch in a minute.")
+        time.sleep(60)
+    sys.exit()
 # And now do it in an endless loop
 try:
     while True:
-        # Make the request
-        response = requests.post(endpoint, data=payload)
-        # Check if the response is different from the previous one
+        try:
+            # Make the request
+            response = requests.post(endpoint, data=payload)
+            # Check if the response is different from the previous one
+        except Exception as e:
+            logger.error(f"An error occurred while making the request: {e}")
+            if args.service >= 1:
+                logger.debug(f"Will retry from scratch in a minute.")
+                time.sleep(60)
+            sys.exit()
         if response.content != prev_response.content:
             prev_values = [int(value) for value in prev_response.content.decode().split("|")[1:-1]]
             values = [int(value) for value in response.content.decode().split("|")[1:-1]]
-            # Check which variables have changed and print their names and new values
+            # Check which variables have changed and create a dictionary with their names and new values
+            changes = {}
             for i, value in enumerate(values):
                 if value != prev_values[i]:
+                    # if value == 1:
+                    #     value = "on"
+                    # else: value = "off"
                     name = list(variables.keys())[i]
-                    print(f"{name} changed to {value}")
+                    changes[name] = value
                     prev_values[i] = value
+            # Convert the dictionary to a JSON string
+            json_payload = json.dumps(changes)
+            # Publish the JSON payload to the MQTT server
+            logger.debug(f"Payload: \n{json_payload}")
+            try:
+                publish.single(MQTT_TOPIC, payload=json_payload, qos=0, retain=False, hostname=MQTT_SERVER, auth={'username':MQTT_USERNAME, 'password':MQTT_PASSWORD})
+            except Exception as e:    
+                logger.error(f"Error publishing to MQTT: {e}")
+                if args.service >= 1:
+                    logger.debug(f"Will retry from scratch in a minute.")
+                    time.sleep(60)
+                sys.exit()
             prev_response = response
 except KeyboardInterrupt:
     sys.exit()
